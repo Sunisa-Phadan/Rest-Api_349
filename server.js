fda78440aa91fe5deeaa67f28052d201e71ed95b
Sunisa-Phadan <Chi99141@gmail.com>
2024-02-16 11:22:28 +0700

    put book-id

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
-
 const bodyParser = require('body-parser')
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -18,10 +16,15 @@ app.get('/books', (req, res) => {
 
 app.get('/books/:id', (req, res) => {
     res.json(books.find(book => book.id === req.params.id))
-  })
+})
 
 app.post('/books', (req, res) => {
   books.push(req.body)
+})
+
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
 })
 
 app.listen(3000, () => {
